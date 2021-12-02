21471d0b3852634bbd1476c138fa122092adeec3
Yash Gautam <72320108+yashgautam9274@users.noreply.github.com>
2021-12-03 00:42:54 +0530

    Update solution.cpp

--- Dug a Bug/Task 1/solution.cpp
+++ Dug a Bug/Task 1/solution.cpp
@@ -1,53 +1,48 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+ 
 using namespace std;
-
+ 
+vector<int> arr[10001];
 vector<int> res;
-vector<int> adj[100001];
-int in[100001];
-
-void kahn(int n)
-{
-    priority_queue<int, vector<int>, greater<int>> q;
-    for (int i = 1; i <= n; i++)
-    {
-        if (in[i] == 0)
-        {
+int in[10001];
+ 
+void kahn(int n){
+   priority_queue<int, vector<int>, greater<int>>q;
+    for(int i=1;i<=n;i++){
+        if(in[i]==0){
             q.push(i);
         }
     }
-    while (!q.empty())
-    {
+    while(!q.empty()){
         int curr = q.top();
-        q.pop();
         res.push_back(curr);
-        for (auto x: adj[curr])
-        {
-            in[x]--;
-            if (in[x] == 0)
-            {
-                q.push(x);
+        q.pop();
+        for(int node : arr[curr]){
+            in[node]--;
+            if(in[node]==0){
+                q.push(node);
             }
         }
     }
-
-    for (auto x: res)
-    {
-        cout << x << " ";
+    int len = res.size();
+    if(len != n){
+        cout<<"Sandro fails."<<"\n";
+        return;
     }
-    cout << endl;
+    for(int i=0;i<len;i++){
+        cout<<res[i]<<" ";
+    }
+    cout<<"\n";
 }
-
-int main()
-{
-    int n, m, x, y;
-    cin >> n >> m;
-    for (int i = 1; i <= m; i++)
-    {
-        cin >> x >> y;
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-        in[x]++;
-        in[y]++;
+ 
+int main(){
+    int n,m,a,b;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        cin>>a>>b;
+        arr[a].push_back(b);
+        in[b]++;
     }
     kahn(n);
-}
+} 
+
